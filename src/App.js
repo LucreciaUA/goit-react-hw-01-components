b494d1acff7b3f310a16fe2f10cfdca8eb1a0b19
LucreciaUA <118127063+LucreciaUA@users.noreply.github.com>
2023-12-03 19:42:46 +0200

    1

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import stats from './data.json'
+import { Statistics } from './stats'
+import { Profile } from './profile'
+import user from './user.json'
+import { Friends } from './friendlist';
+import friend from './friends.json'
+import { Table } from './transaction';
+import items from './transactions.json'
+
+const App = () => {
+  return (
+    <>
+      <Profile user={user} />
+      <Statistics stats={stats} />
+      <Friends friend={friend} />
+      <Table items={items} />
+    </>
+  )
+}
+
+export default App
---
+++ src/index.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+//import App from './App';
+import user from './user.json';
+import reportWebVitals from './reportWebVitals';
+import { Profile } from './profile';
+import App from './App.js'
+import stats from './data.json'
+import { Statistics } from './stats'
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+
+
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
